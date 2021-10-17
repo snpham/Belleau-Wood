15cc978855062f9649711e99242b2fffd0c5bae5
Son <snpham90@gmail.com>
2021-10-16 20:06:22 -0500

    cleaning up logistic regression

--- scripts/logistic_regression.py
+++ scripts/logistic_regression.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 from collections import Counter
 import re
 
@@ -9,17 +8,33 @@ def z_value(w, x, b):
 
 
 def sigmoid(z):
-    # print('z value:', z)
+    """sigmoid function to squash the output of the 
+    linear equation itno a range of [0,1]
+    """
     return 1 / (1+ np.exp(-z))
 
 
 def cross_entropy_loss(y_est, y):
+    """logarithmic loss function to calculate the cost for 
+    misclassifying
+    :param y_est: estimated label
+    :param y: correct label
+    :return: cross entroy loss
+    """
     return -(y * np.log(y_est) + (1-y)*np.log(1-y_est))
 
 
 def cross_entropy(w, x, b, y):
+    """logarithmic loss function to calculate the cost for 
+    misclassifying
+    :param w: weight vector
+    :param x: input vector
+    :param b: bias
+    :param y: correct label
+    :return L_CE: cross entroy loss
+    """
     sigma = sigmoid(z_value(w, x, b))
-    L_CE = -(y * np.log(sigma) + (1-y)*np.log(1-sigma))
+    L_CE = cross_entropy_loss(y_est=sigma, y=y)
     return L_CE
 
 
@@ -29,7 +44,7 @@ def gradient(x, y, w, b):
 
     del_L = [(sigmoid(z)-y)*xi for xi in x]
 
-    del_b = sigmoid(z)-y
+    del_b = sigmoid(z)-y # ?
     del_L.append(del_b)
 
     return np.array(del_L)
@@ -62,93 +77,60 @@ def softmax(z):
 
 def run_tests():
 
-    # # testing sigmoid
-    # w = np.array([2.5, -5.0, -1.2, 0.5, 2.0, 0.7])
-    # x = np.array([3, 2, 1, 3, 0, 4.15])
-    # b = 0.1
-    # P_pos = sigmoid(z=z_value(w, x, b))
+    # testing sigmoid and cross entropy
+    w = np.array([2.5, -5.0, -1.2, 0.5, 2.0, 0.7])
+    x = np.array([3, 2, 1, 3, 0, 4.15])
+    b = 0.1
+    P_pos = sigmoid(z=z_value(w, x, b))
     # print(z_value(w, x, b))
-    # P_neg = 1 - P_pos
-    # assert np.allclose([P_pos, P_neg], [0.69104, 0.30895], rtol=1e-2)
+    P_neg = 1 - P_pos
+    assert np.allclose([P_pos, P_neg], [0.69104, 0.30895], rtol=1e-2)
+    # cross entropy
+    y = 1
+    c_ent = cross_entropy(w, x, b, y)
+    assert np.allclose(c_ent, 0.369, rtol=1e-2)
 
-
-    # y = 1
-    # c_ent = cross_entropy(w, x, b, y)
-    # print(c_ent)
-    # assert np.allclose(c_ent, 0.369, rtol=1e-2)
-
-    
     # testing gradient and stochastic gradient
     x = [3,2]
     y = 1
     w = [0, 0]
-    b = 0
-    nu = 0.1
+    b = 0.0
     grad = gradient(x, y, w, b)
     assert np.allclose(grad, [-1.5, -1.0, -0.5], rtol=1e-2 )
-
+    nu = 0.1
     stoch = stochastic_grad(x, y, w, b, nu)
     assert np.allclose(stoch, [0.15, 0.1, 0.05], rtol=1e-2 )
-    print(stoch)
     stoch = stochastic_grad_constb(x, y, w, b, nu)
-    print(stoch)
+    assert np.allclose(stoch, [0.15, 0.1, 0.0], rtol=1e-2 )
+
+    # testing softmax
+    z = [0.6, 1.1, -1.5, 1.2, 3.2, -1.1]
+    sm = softmax(z)
+    assert np.allclose(sm, 
+        [0.05482, 0.09039, 0.006713, 0.0998, 0.7381, 0.01001], rtol=1e-2)
+
+    # test from lecture 9/21 - not sure how they got updated weights
+    w = np.array([2.5, -5.0, -1.2, 0.5, 2.0, 0.7])
+    x = np.array([3, 2, 1, 3, 0, 4.15])
+    b = 0.1
+    y = 1
+    P_pos = sigmoid(z=z_value(w, x, b))
+    assert np.allclose(P_pos, 0.69104, rtol=1e-2)
+    grad = gradient(x, y, w, b)
+
+    assert np.allclose(grad, [-0.9268, -0.6179, -0.3089, -0.9268, 
+                              -0., -1.282, -0.3089], rtol=1e-2)
+
+    nu = 1
+    w_new = update_weight(w, b, nu, grad)
+    assert np.allclose(w_new, [3.426, -4.382, -0.891, 1.426, 2., 1.982, 0.408], rtol=1e-2)
+    z = z_value(w=w_new[:-1], x=x, b=b)
+    sig = sigmoid(z=z)
+    print(sig)
 
 
-    # # testing softmax
-    # z = [0.6, 1.1, -1.5, 1.2, 3.2, -1.1]
-    # sm = softmax(z)
-    # assert np.allclose(sm, 
-    #     [0.05482, 0.09039, 0.006713, 0.0998, 0.7381, 0.01001], rtol=1e-2)
-
-
-    # # test from lecture 9/21 - not sure how he got updated weights
-    # w = np.array([2.5, -5.0, -1.2, 0.5, 2.0, 0.7])
-    # x = np.array([3, 2, 1, 3, 0, 4.15])
-    # b = 0.1
-    # y = 1
-    # P_pos = sigmoid(z=z_value(w, x, b))
-    # assert np.allclose(P_pos, 0.69104, rtol=1e-2)
-
-    # grad = gradient(x, y, w, b)
-    # assert np.allclose(grad, [-0.9268, -0.6179, -0.3089, -0.9268, -0., -1.282, -0.3089], rtol=1e-2)
-
-    # nu = 1
-    # w_new = update_weight(w, b, nu, grad)
-    # assert np.allclose(w_new, [3.426, -4.382, -0.891, 1.426, 2., 1.982, 0.408], rtol=1e-2)
-    # print(w_new[:-1])
-    # z = z_value(w=w_new[:-1], x=x, b=b)
-    # sig = sigmoid(z=z)
-    # print(sig)
-
-    # # test from lecture 9/21
-    # w = np.array([0., 0., 0., 0., 0., 0.])
-    # x = np.array([3, 2, 1, 3, 0, 4.15])
-    # b = 0.1
-    # y = 1
-    # P_pos = sigmoid(z=z_value(w, x, b))
-    # assert np.allclose(P_pos, 0.5249, rtol=1e-2)
-
-    # # grad = gradient(x, y, w, b)
-    # # # assert np.allclose(grad, [-0.9268, -0.6179, -0.3089, -0.9268, -0., -1.282, -0.3089], rtol=1e-2)
-    # # print(grad)
-    # # nu = 1
-    # # w_new = update_weight(w, b, nu, grad)
-    # # # assert np.allclose(w_new, [3.426, -4.382, -0.891, 1.426, 2., 1.982, 0.408], rtol=1e-2)
-    # # print(w_new[:-1])
-    # # z = z_value(w=w_new[:-1], x=x, b=b)
-    # # sig = sigmoid(z=z)
-    # # print(sig)
-
-
-    # # x = [-3, 1, 4, 1]
-    # # w = [1, 1, 1, 1]
-    # # y = 1
-    # # b = 0
-    # # grad = gradient(x, y, w, b)
-    # # print(grad)
-
-    # l_ce = cross_entropy_loss(y_est=0.4, y=1)
-    # print(l_ce)
+    l_ce = cross_entropy_loss(y_est=0.4, y=1)
+    print(l_ce)
 
 
 def extraction(review, pos_words, neg_words, pronouns):
@@ -187,10 +169,7 @@ def extraction(review, pos_words, neg_words, pronouns):
     return [counts, np.log(len(words))]
 
 
-if __name__ == '__main__':
-    pass
-    # run_tests()
-
+def hotel_ratings_model():
     # get positive and negative reviews from csv's
     review_file_pos = np.loadtxt('datasets/assignment2/hotelPosT-train.txt', 
                                  delimiter='\t', dtype='str', encoding="utf8")
@@ -243,7 +222,6 @@ if __name__ == '__main__':
     reviews_file = np.loadtxt('datasets/assignment2/pham-son-assgn2-part1.csv', 
                               delimiter=',', encoding="utf8", 
                               dtype='str')
-    # print(reviews_file)
 
     # TEST SET: do same for test set
     with open('datasets/assignment2/pham-son-assgn2-part1-testset.csv', 'w')as f:
@@ -258,7 +236,6 @@ if __name__ == '__main__':
     test_reviews_file = np.loadtxt('datasets/assignment2/pham-son-assgn2-part1-testset.csv', 
                                    delimiter=',', encoding="utf8", 
                                    dtype='str')
-    # print(test_reviews_file)
 
     # get file ids and vectors
     ids = reviews_file[:,0]
@@ -276,7 +253,7 @@ if __name__ == '__main__':
 
     # generate training and testing set
     rands = np.random.rand(vectors_w_bias.shape[0])
-    splitpoint = rands < np.percentile(rands, 100)
+    splitpoint = rands < np.percentile(rands, 80)
     train_x = np.array(vectors_w_bias[splitpoint])
     train_y = np.array(ids[splitpoint])
     dev_x = np.array(vectors_w_bias[~splitpoint])
@@ -303,9 +280,8 @@ if __name__ == '__main__':
 
     # run learning model
     theta = [0,0,0,0,0,0,0]
-    thetas = []
     nu = 1
-    why = 1
+    bias = 1
     for k in range(0, 2000):
         random_idx = np.random.choice(train_x.shape[0], 
                                       size=1, replace=True)
@@ -318,7 +294,7 @@ if __name__ == '__main__':
         theta = stochastic_grad_constb(x=random_sample[:-1], 
                                        y=y_true, 
                                        w=theta[:-1], 
-                                       b=why, 
+                                       b=bias, 
                                        nu=nu)
 
     # # run dev set
@@ -344,6 +320,14 @@ if __name__ == '__main__':
             elif sigma >= 0.5:
                 classf = 'POS'
             f.write(f'{id}\t{classf}\n')
+
+
+
+if __name__ == '__main__':
+    pass
+    run_tests()
+
+    # hotel_ratings_model()
 
 
 
